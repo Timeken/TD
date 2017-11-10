f50905eabdf9b601039809e8447f70c7860b638f
Timeken <Tim@ekenberg.net>
2017-11-10 12:01:49 +0100

    V0.2.1

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour {
     [SerializeField]
     public int HP, DMG;
     
     // Use this for initialization
-    void Start()
+    public void Start()
     {
-
+        GameObject goal = GameObject.Find("Goal");
+        if (goal)
+        {
+            GetComponent<NavMeshAgent>().destination = goal.transform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter(Collider collider)
     {
- 
+        if (collider.name == "Goal")
+        {
+            //TODO decreas goal health here.
+            Destroy(gameObject);
+        }
+        else if (collider.name == "projectile")
+        {
+            //TODO get dmg from projectile and decrese enemy hp.
+        }
     }
 }
