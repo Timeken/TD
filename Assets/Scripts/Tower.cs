605c172338a7befb815241a8ed98d82be056068c
Timeken <Tim@ekenberg.net>
2017-11-13 14:14:24 +0100

    V.0.5.4

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+    public int Cost, DMG, Firerate, Range, Rotation;
+
+
+}
---
+++ Assets/Scripts/TowerBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : Tower {
+
+    void Update()
+    {
+        Rotation = 35;
+        transform.Rotate(Vector3.up * Time.deltaTime * Rotation, Space.World);
+    }
+}
