d91a7e89f3cd77779752c16c944591e9db483efe
HanniBun <johannanas.nilsson@gmail.com>
2017-11-13 20:35:07 +0100

    V0.4.5
    Gave Minion1 and Minion2 classes a SetValues method to easily adjust their starting hp and damage values.
    
    Also, added hp and damage to the instantiation of enemies

---
+++ Assets/Scripts/Hpbar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hpbar : MonoBehaviour
+{
+
+    public Minion1 minion;
+    public int minionHP
+    {
+        get { return minion.HP; }
+        set { minion.HP = value; }
+    }
+
+    public void Test()
+    {
+    }
+}
--- Assets/Scripts/Minion1.cs
+++ Assets/Scripts/Minion1.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Minion1 : Enemy {
-    
+    public void SetValues(int hp, int damage)
+    {
+        this.HP = hp;
+        this.DMG = damage;
+    }
 }
